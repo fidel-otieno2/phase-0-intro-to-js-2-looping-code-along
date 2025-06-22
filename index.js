59c8fdf516cff563ce095a6f36893cbbe8ab19c5
fidel-otieno2 <fidelmartins05@gmail.com>
2025-06-22 23:22:22 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+[
+  "Thank you, Charlie, for the wonderful birthday gift!",
+  "Thank you, Samip, for the wonderful birthday gift!",
+  "Thank you, Ali, for the wonderful birthday gift!"
+]
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+countDown(10);
